6af7a79380f73712198a104a39490a1a9720430f
jonghwa <pincoins@gmail.com>
2021-02-25 19:03:58 +0700

    home page index

--- conf/urls.py
+++ conf/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
+    path('',
+         views.HomeView.as_view(), name='home'),
+
     path('admin/', admin.site.urls),
 ]
